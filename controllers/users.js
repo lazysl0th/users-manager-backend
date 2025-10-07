24b32333a89cf642dd5014753ba0afbdbf33ef22
lazy_sl0th <anton.vlrch.ch@gmail.com>
2025-10-07 16:11:02 +0400

    fix cookies

--- controllers/users.js
+++ controllers/users.js
@@ -193,4 +193,10 @@ export const changePassword = async (req, res, next) => {
   }
 }
 
-export const logout = (req, res) => res.status(OK.statusCode).clearCookie('token').send({});
+export const logout = (req, res) => res.status(OK.statusCode).cookie('token', {
+  expires: Date.now(),
+  httpOnly: true, 
+  secure: true, 
+  sameSite: 'none', 
+  path: '/',
+}).send({});
